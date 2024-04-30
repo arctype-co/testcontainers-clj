c9cffc8f79d1e4f71f19b528cf7bf1b369aef8df
Ryan Sundberg <ryan@arctype.co>
2024-04-30 11:08:45 -0700

    with-network: Network test fixture function

--- src/clj_test_containers/core.clj
+++ src/clj_test_containers/core.clj
@@ -30,6 +30,8 @@
      BindMode/READ_WRITE
      BindMode/READ_ONLY)))
 
+(def ^:dynamic *network* nil)
+
 (defonce started-instances (atom #{}))
 
 (defmulti wait
@@ -403,6 +405,16 @@
       (.removeNetworkCmd (:id instance))
       (.exec))
   instance)
+
+(defn with-network
+  ([] (with-network {}))
+  ([network-options]
+   (fn [test-fn]
+     (binding [*network* (create-network network-options)]
+       (try
+         (test-fn)
+         (finally
+           (remove-network! *network*)))))))
 
 (defn- stop-and-remove-container! [instance]
   (let [docker-client (DockerClientFactory/instance)]
